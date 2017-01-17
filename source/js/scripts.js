df8cdef50315047785b875be50905a4ab2769829
Claudio Mendonca <claudioccm@gmail.com>
2017-01-16 22:10:18 -0200

    index-1

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -20,57 +20,64 @@ $(document).ready(function () {
         return true;
     });
 
+
+    $('.portfolio-gallery').isotope({
+      // options
+      itemSelector: '.portfolio-item',
+      layoutMode: 'masonry'
+    });
+
     // Modal Click Behavior
-    $('.js-open-modal').click(function () {
-        $('.js-target-modal').addClass('js-active');
-        $('#overlay').addClass('js-active');
-        $('body').addClass('js-body-modal-active');
-    });
+    // $('.js-open-modal').click(function () {
+    //     $('.js-target-modal').addClass('js-active');
+    //     $('#overlay').addClass('js-active');
+    //     $('body').addClass('js-body-modal-active');
+    // });
 
-    $('.js-close-modal').click(function () {
-        $('.js-target-modal').removeClass('js-active');
-        $('#overlay').removeClass('js-active');
-        $('body').removeClass('js-body-modal-active');
-    });
+    // $('.js-close-modal').click(function () {
+    //     $('.js-target-modal').removeClass('js-active');
+    //     $('#overlay').removeClass('js-active');
+    //     $('body').removeClass('js-body-modal-active');
+    // });
 
-    // Sticky Click Behavior
-    $('.js-close-sticky').click(function () {
-        $('.js-target-sticky').removeClass('js-active');
-    });
+    // // Sticky Click Behavior
+    // $('.js-close-sticky').click(function () {
+    //     $('.js-target-sticky').removeClass('js-active');
+    // });
 
-    // Search Click Behavior
-    $('.js-trigger-search').click(function (e) {
-        e.preventDefault();
-        $(this).parent().addClass('js-active');
-        $('#overlay').addClass('js-active');
-    });
+    // // Search Click Behavior
+    // $('.js-trigger-search').click(function (e) {
+    //     e.preventDefault();
+    //     $(this).parent().addClass('js-active');
+    //     $('#overlay').addClass('js-active');
+    // });
 
-    // Main Menu Click Behavior
-    $('.js-trigger-menu').click(function (e) {
-        $(this).next().addClass('js-active-menu');
-        $('#overlay').addClass('js-active');
-    });
+    // // Main Menu Click Behavior
+    // $('.js-trigger-menu').click(function (e) {
+    //     $(this).next().addClass('js-active-menu');
+    //     $('#overlay').addClass('js-active');
+    // });
 
-    // General Click Behavior for Overlay
-    $('#overlay').click(function () {
-        $('.js-active').removeClass('js-active');
-        $('.js-active-menu').removeClass('js-active-menu');
-    });
+    // // General Click Behavior for Overlay
+    // $('#overlay').click(function () {
+    //     $('.js-active').removeClass('js-active');
+    //     $('.js-active-menu').removeClass('js-active-menu');
+    // });
 
-    // Slider
-    $('.slider').slick({
-        arrows: true,
-        draggable: false,
-        swipeToSlide: true,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        responsive: [
-            {
-                breakpoint: 800,
-                settings: {
-                    draggable: true
-                }
-            }
-        ]
-    });
+    // // Slider
+    // $('.slider').slick({
+    //     arrows: true,
+    //     draggable: false,
+    //     swipeToSlide: true,
+    //     autoplay: true,
+    //     autoplaySpeed: 3000,
+    //     responsive: [
+    //         {
+    //             breakpoint: 800,
+    //             settings: {
+    //                 draggable: true
+    //             }
+    //         }
+    //     ]
+    // });
 }); // doc.ready
